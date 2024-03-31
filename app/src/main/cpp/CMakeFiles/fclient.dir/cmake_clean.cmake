dc1d732dd2ae55134a3174e0d9973f1356a63203
Tsibulskaya <p.tsibulskaya@yandex.ru>
2024-03-31 20:31:00 +0300

    lr1

---
+++ app/src/main/cpp/CMakeFiles/fclient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fclient.dir/native-lib.cpp.o"
+  "CMakeFiles/fclient.dir/native-lib.cpp.o.d"
+  "libfclient.pdb"
+  "libfclient.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ app/src/main/cpp/CMakeFiles/fclient.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for fclient.
+# This may be replaced when dependencies are built.
