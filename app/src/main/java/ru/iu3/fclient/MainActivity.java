9d25cfdf6a9846ed6d741e41eb6609cccfdc8164
Tsibulskaya <p.tsibulskaya@yandex.ru>
2024-03-31 20:35:00 +0300

    lr2

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,9 +1,22 @@
 package ru.iu3.fclient;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
@@ -17,23 +30,72 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    ActivityResultLauncher activityResultLauncher;
+
+    public static byte[] stringToHex(String s) {
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex) {
+            hex = null;
+        }
+        return hex;
+    }
+
+    public void onButtonClick(View v) {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+//        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+//        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+//        byte[] dec = decrypt(key, enc);
+//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+//        Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback () {
+                    @Override
+                    public void onActivityResult(Object obj) {
+                        if (obj instanceof ActivityResult) {
+                            ActivityResult result = (ActivityResult) obj;
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+                                Intent data = result.getData();
+                                // обработка результата
+                                String pin = data.getStringExtra("pin");
+                                Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
 
-        int res = initRng();
-        byte[] v = randomBytes(16);
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-        String testString = "Hello";
-        byte[] testByteArray = testString.getBytes();
-        byte[] encryptedByteArray = encrypt(v, testByteArray);
-        byte[] decryptedByteArray = decrypt(v, encryptedByteArray);
-        String decryptedString = new String(decryptedByteArray);
-
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+//        int res = initRng();
+//        byte[] v = randomBytes(16);
+//
+//        String testString = "Hello";
+//        byte[] testByteArray = testString.getBytes();
+//        byte[] encryptedByteArray = encrypt(v, testByteArray);
+//        byte[] decryptedByteArray = decrypt(v, encryptedByteArray);
+//        String decryptedString = new String(decryptedByteArray);
+//
+//        // Example of a call to a native method
+//        TextView tv = binding.sampleText;
+//        tv.setText(stringFromJNI());
     }
 
 //    @Override
